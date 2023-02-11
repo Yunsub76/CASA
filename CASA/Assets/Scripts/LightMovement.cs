dc9aed8d20d27015e3b9c3894545ac155f375a73
Yunsub76 <oyeoye376@gmail.com>
2023-02-11 14:15:35 +0900

    빛 색감 변경

--- CASA/Assets/Scripts/LightMovement.cs
+++ CASA/Assets/Scripts/LightMovement.cs
@@ -7,15 +7,34 @@ public class LightMovement : MonoBehaviour {
 	public float lightH;
 	bool loc = true;
 	bool locH = true;
-	// Use this for initialization
+	public bool mood = true;
+	public bool moodchange = true;
+	Light lightColor;
+	Color color;
+	float PMoodCr;
+	float PMoodCg;
+	float PMoodCb;
+	float NMoodCr;
+	float NMoodCg;
+	float NMoodCb;
+
+
 	void Start () {
-		
+		lightColor = GetComponent<Light>();
+		lightColor.color = new Color(0.1960784f, 0.7843137f, 0.7843137f, 1f);
+		PMoodCr = 0.3921569f;
+		PMoodCg = 0.5882353f;
+		PMoodCb = 1f;
+		NMoodCr = 0f;
+		NMoodCg = 0.5882353f;
+		NMoodCb = 0f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (transform.position.x >= 250 )
+		//Limit the moving range of light
+		if (transform.position.x >= 250 ) 
 		{
 			loc = false;
 		}
@@ -23,14 +42,7 @@ public class LightMovement : MonoBehaviour {
 		{
 			loc = true;
 		}
-		if (loc == true)
-		{
-			transform.position = new Vector3(transform.position.x + lightS, transform.position.y, transform.position.z);
-		}
-		else if(loc == false)
-        {
-			transform.position = new Vector3(transform.position.x - lightS, transform.position.y, transform.position.z);
-		}
+		
 		if (transform.position.y >= 100)
 		{
 			locH = false;
@@ -39,6 +51,21 @@ public class LightMovement : MonoBehaviour {
 		{
 			locH = true;
 		}
+		if (moodchange == true) Color();
+		//Light Movement function 
+		Transform();
+	}
+
+	void Transform()
+    {
+		if (loc == true)
+		{
+			transform.position = new Vector3(transform.position.x + lightS, transform.position.y, transform.position.z);
+		}
+		else if (loc == false)
+		{
+			transform.position = new Vector3(transform.position.x - lightS, transform.position.y, transform.position.z);
+		}
 		if (locH == true)
 		{
 			transform.position = new Vector3(transform.position.x, transform.position.y + lightH, transform.position.z);
@@ -46,6 +73,21 @@ public class LightMovement : MonoBehaviour {
 		else if (locH == false)
 		{
 			transform.position = new Vector3(transform.position.x, transform.position.y - lightH, transform.position.z);
+		}
+	}
+	void Color()
+    {
+		if (mood == true)
+		{
+			if (lightColor.color.r < PMoodCr) lightColor.color = new Color(lightColor.color.r + 0.001f, lightColor.color.g, lightColor.color.b);
+			else if (lightColor.color.g > PMoodCg) lightColor.color = new Color(lightColor.color.r, lightColor.color.g - 0.001f, lightColor.color.b);
+			else if (lightColor.color.b > PMoodCb) lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b + 0.001f);
+		}
+		else if (mood == false)
+		{
+			if (lightColor.color.r > NMoodCr) lightColor.color = new Color(lightColor.color.r - 0.001f, lightColor.color.g, lightColor.color.b);
+			else if (lightColor.color.g > NMoodCg) lightColor.color = new Color(lightColor.color.r, lightColor.color.g - 0.001f, lightColor.color.b);
+			else if (lightColor.color.b > NMoodCb) lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b - 0.001f);
 		}
 	}
 }
