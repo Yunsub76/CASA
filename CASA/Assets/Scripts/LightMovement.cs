e9acae446a10656ddeac2fc8ad7edcd4552f7c43
Yunsub76 <oyeoye376@gmail.com>
2023-02-11 16:14:55 +0900

    타이머 및 빛 색 변경

--- CASA/Assets/Scripts/LightMovement.cs
+++ CASA/Assets/Scripts/LightMovement.cs
@@ -7,28 +7,20 @@ public class LightMovement : MonoBehaviour {
 	public float lightH;
 	bool loc = true;
 	bool locH = true;
-	public bool mood = true;
-	public bool moodchange = true;
+	public bool positive = true;
 	Light lightColor;
 	Color color;
-	float PMoodCr;
-	float PMoodCg;
-	float PMoodCb;
-	float NMoodCr;
-	float NMoodCg;
-	float NMoodCb;
+	public Color positiveTargetColor;
+	public Color negativeTargetColor;
+	public float colorSpeed;
+	bool gm; 
 
 
 	void Start () {
+		gm = GameObject.Find("GameManager").GetComponent<SliderTimer>().stopTimer;
 		lightColor = GetComponent<Light>();
-		lightColor.color = new Color(0.1960784f, 0.7843137f, 0.7843137f, 1f);
-		PMoodCr = 0.3921569f;
-		PMoodCg = 0.5882353f;
-		PMoodCb = 1f;
-		NMoodCr = 0f;
-		NMoodCg = 0.5882353f;
-		NMoodCb = 0f;
-
+		lightColor.color = new Color(0.1960784f, 0.7843137f, 0.7843137f, 1f); // Basic Color
+		
 	}
 	
 	// Update is called once per frame
@@ -51,43 +43,40 @@ public class LightMovement : MonoBehaviour {
 		{
 			locH = true;
 		}
-		if (moodchange == true) Color();
+		if (gm == false) Color();
 		//Light Movement function 
 		Transform();
 	}
 
 	void Transform()
     {
-		if (loc == true)
-		{
-			transform.position = new Vector3(transform.position.x + lightS, transform.position.y, transform.position.z);
-		}
-		else if (loc == false)
-		{
-			transform.position = new Vector3(transform.position.x - lightS, transform.position.y, transform.position.z);
-		}
-		if (locH == true)
-		{
-			transform.position = new Vector3(transform.position.x, transform.position.y + lightH, transform.position.z);
-		}
-		else if (locH == false)
-		{
-			transform.position = new Vector3(transform.position.x, transform.position.y - lightH, transform.position.z);
-		}
+		if (loc == true)transform.position = new Vector3(transform.position.x + lightS, transform.position.y, transform.position.z);
+		else if (loc == false)transform.position = new Vector3(transform.position.x - lightS, transform.position.y, transform.position.z);
+
+		if (locH == true)transform.position = new Vector3(transform.position.x, transform.position.y + lightH, transform.position.z);
+		else if (locH == false)transform.position = new Vector3(transform.position.x, transform.position.y - lightH, transform.position.z);
+		
 	}
 	void Color()
     {
-		if (mood == true)
+
+		if (positive == true) // positiveTargetColor색으로 변경
 		{
-			if (lightColor.color.r < PMoodCr) lightColor.color = new Color(lightColor.color.r + 0.001f, lightColor.color.g, lightColor.color.b);
-			else if (lightColor.color.g > PMoodCg) lightColor.color = new Color(lightColor.color.r, lightColor.color.g - 0.001f, lightColor.color.b);
-			else if (lightColor.color.b > PMoodCb) lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b + 0.001f);
+			if (lightColor.color.r <= positiveTargetColor.r) lightColor.color = new Color(lightColor.color.r + colorSpeed, lightColor.color.g, lightColor.color.b);
+			else lightColor.color = new Color(lightColor.color.r - colorSpeed, lightColor.color.g, lightColor.color.b);
+			if (lightColor.color.g <= positiveTargetColor.g) lightColor.color = new Color(lightColor.color.r, lightColor.color.g + colorSpeed, lightColor.color.b);
+			else lightColor.color = new Color(lightColor.color.r, lightColor.color.g - colorSpeed, lightColor.color.b);
+			if (lightColor.color.b <= positiveTargetColor.b) lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b + colorSpeed);
+			else lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b - colorSpeed);
 		}
-		else if (mood == false)
+		else if (positive == false)  // negativeTargetColor색으로 변경
 		{
-			if (lightColor.color.r > NMoodCr) lightColor.color = new Color(lightColor.color.r - 0.001f, lightColor.color.g, lightColor.color.b);
-			else if (lightColor.color.g > NMoodCg) lightColor.color = new Color(lightColor.color.r, lightColor.color.g - 0.001f, lightColor.color.b);
-			else if (lightColor.color.b > NMoodCb) lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b - 0.001f);
+			if (lightColor.color.r <= negativeTargetColor.r) lightColor.color = new Color(lightColor.color.r + colorSpeed, lightColor.color.g, lightColor.color.b);
+			else lightColor.color = new Color(lightColor.color.r - colorSpeed, lightColor.color.g, lightColor.color.b);
+			if (lightColor.color.g <= negativeTargetColor.g) lightColor.color = new Color(lightColor.color.r, lightColor.color.g + colorSpeed, lightColor.color.b);
+			else lightColor.color = new Color(lightColor.color.r, lightColor.color.g - colorSpeed, lightColor.color.b);
+			if (lightColor.color.b <= negativeTargetColor.b) lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b + colorSpeed);
+			else lightColor.color = new Color(lightColor.color.r, lightColor.color.g, lightColor.color.b - colorSpeed);
 		}
 	}
 }
--- CASA/Assets/Scripts/SliderTimer.cs
+++ CASA/Assets/Scripts/SliderTimer.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SliderTimer : MonoBehaviour {
 
-	// Use this for initialization
+
+	public Slider timerSlider;
+	public Text timertext;
+	public float gameTime;
+
+	public bool stopTimer;
+
 	void Start () {
-		
+		stopTimer = false;
+		timerSlider.maxValue = gameTime;
+		timerSlider.value = gameTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float time = gameTime - Time.time;
+		int minutes = Mathf.FloorToInt(time / 60);
+		int seconds = Mathf.FloorToInt(time - minutes * 60f);
+
+		string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+		if(time <= 0)
+        {
+			stopTimer = true;
+        }
+		if(stopTimer == false)
+        {
+			timertext.text = textTime;
+			timerSlider.value = time;
+        }
 	}
 }
