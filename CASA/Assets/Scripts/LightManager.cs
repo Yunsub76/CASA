2c8b0a090332ecd1f99875b2b0ffabb2df6b7f32
Yunsub76 <oyeoye376@gmail.com>
2023-02-13 15:18:40 +0900

    Create LightManager

---
+++ CASA/Assets/Scripts/LightManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+	public static LightManager instance;
+	public List<Light> lights;
+	public List<Color> positiveTargetColor;
+	public List<Color> NegativeTargetColor;
+	public float lightIntensity;
+
+	// Use this for initialization
+	void Start()
+	{
+		foreach (Light light in lights)
+		{
+			light.intensity = lightIntensity;
+		}
+
+	}
+
+	// Update is called once per frame
+	void LightMovementCall()
+	{
+
+	}
+}
