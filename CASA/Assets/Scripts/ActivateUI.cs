bbc8e813dff58f4573954d8fd9e40a9dae1b0c46
Yunsub76 <oyeoye376@gmail.com>
2023-02-14 14:36:24 +0900

    Create Final Score

---
+++ CASA/Assets/Scripts/ActivateUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateUI : MonoBehaviour {
+
+	public bool activateUI = false;
+    GameObject image;
+    GameObject slider;
+    GameObject scoreText;
+
+	void Awake()
+    {
+        image = GameObject.Find("Image");
+        slider = GameObject.Find("Slider");
+        scoreText = GameObject.Find("ScoreText");
+
+    }
+
+    void Update()
+    {
+        if (activateUI == true)
+            UIActivate();
+        else
+            UIDisabled();
+    }
+
+	public void UIDisabled()
+    {
+        image.SetActive(false);
+        slider.SetActive(false);
+        scoreText.SetActive(false);
+    }
+
+    public void UIActivate()
+    {
+        image.SetActive(true);
+        slider.SetActive(true);
+        scoreText.SetActive(true);
+    }
+
+}
