5703029214d7cab891b804c509384220965b84ca
Hyeonjin20 <22000205@handong.ac.kr>
2023-02-14 19:52:45 +0900

    UI 수정

---
+++ CASA/Assets/Scripts/UI_blinkText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_blinkText : MonoBehaviour {
+	public Text flashingText;
+
+	// Use this for initialization
+	void Start () {
+		string animText = flashingText.text;
+		flashingText.text = "";
+		StartCoroutine(BlinkText(animText));
+	}
+	
+	public IEnumerator BlinkText(string text){
+		while(true){
+			flashingText.text = "";
+			yield return new WaitForSeconds (.5f);
+			flashingText.text = text;
+			yield return new WaitForSeconds (.5f);
+		}
+	}
+}
