02fd91da9403bc23efca8cc8890bbbeac6553ecd
LyoungSung <21800539@handong.ac.kr>
2023-02-18 14:30:06 +0900

    좌우 제스쳐 기능 추가(실험 필요)

--- CASA/Assets/Scripts/HandTracker.cs
+++ CASA/Assets/Scripts/HandTracker.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class HandTracker : MonoBehaviour {
 
-	//vertical tracking
+	//For vertical tracking values
 	List<Vector3> filterWindow = new List<Vector3>();
-	float filter_elapsed_time = 0.0f;
+	 //float filter_elapsed_time = 0.0f;
 	public int filter_window_count = 5;
 
 	List<Vector3> window = new List<Vector3>();
 	float prev_y = 0.0f;
-	float elapsed_time = 0.0f;
+	 //float elapsed_time = 0.0f;
 	int zeroCrossings = 0;
 	public int window_count = 10;
 
 	public float minimun_height = 0.2f;
+
+	//For Horizontal tracking values
+	List<Vector3> filterWindow_H = new List<Vector3>();
+	 //float filter_elapsed_time = 0.0f;
+	public int filter_window_H_count = 5;
+
+	List<Vector3> window_H = new List<Vector3>();
+	float prev_x = 0.0f;
+	 //float elapsed_time = 0.0f;
+	int zeroCrossings_H = 0;
+	public int window_H_count = 10;
+
+	public float minimun_width = 0.2f;
 
 	// Use this for initialization
 	void Start () {
@@ -35,30 +48,35 @@ public class HandTracker : MonoBehaviour {
 	public void Init() {
 		window.Clear();
 		filterWindow.Clear();
-		elapsed_time = 0.0f;
+		 //elapsed_time = 0.0f;
 		zeroCrossings = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
+
+	void VerticalTracking()
+    {
 		Vector3 currPosition = transform.position;
 		window.Add(currPosition);
-		elapsed_time += Time.deltaTime;
+		//elapsed_time += Time.deltaTime;
 
 		filterWindow.Add(currPosition);
-		filter_elapsed_time += Time.deltaTime;
+		//filter_elapsed_time += Time.deltaTime;
 
 		float curr_y = 0.0f;
 		float min_y = 10000.0f;
 		float max_y = -10000.0f;
-		if(filterWindow.Count > filter_window_count)
+		if (filterWindow.Count > filter_window_count)
 		{
 			int n = filterWindow.Count;
-			foreach(var item in filterWindow)
+			foreach (var item in filterWindow)
 			{
 				curr_y += item.y;
-				if(min_y > item.y) min_y = item.y;
-				if(max_y < item.y) max_y = item.y;
+				if (min_y > item.y) min_y = item.y;
+				if (max_y < item.y) max_y = item.y;
 			}
 			curr_y /= n;
 			filterWindow.RemoveAt(0);
@@ -66,11 +84,11 @@ public class HandTracker : MonoBehaviour {
 		float height = max_y - min_y;
 
 
-		if(window.Count > window_count && height > minimun_height)
+		if (window.Count > window_count && height > minimun_height)
 		{
 			float mean_y = 0.0f;
 			int n = window.Count;
-			foreach(var item in window)
+			foreach (var item in window)
 			{
 				mean_y += item.y;
 			}
@@ -79,14 +97,67 @@ public class HandTracker : MonoBehaviour {
 			float sign1 = GetSign(prev_y, mean_y);
 			float sign2 = GetSign(curr_y, mean_y);
 
-			if(sign1 != sign2) {
+			if (sign1 != sign2)
+			{
 				zeroCrossings += 1;
 			}
 
 			window.RemoveAt(0);
 		}
-		Debug.Log(filter_elapsed_time +"/" + filterWindow.Count + "/" + min_y + "/" + max_y + "/" + height + "/" + zeroCrossings);
+		//Debug.Log(filterWindow.Count + "/" + min_y + "/" + max_y + "/" + height + "/" + zeroCrossings);
 
 		prev_y = curr_y;
+	}
+
+	void HorizontalTracking()
+	{
+		Vector3 currPosition = transform.position;
+		window_H.Add(currPosition);
+		//elapsed_time += Time.deltaTime;
+
+		filterWindow_H.Add(currPosition);
+		//filter_elapsed_time += Time.deltaTime;
+
+		float curr_x = 0.0f;
+		float min_x = 10000.0f;
+		float max_x = -10000.0f;
+		if (filterWindow_H.Count > filter_window_H_count)
+		{
+			int n = filterWindow_H.Count;
+			foreach (var item in filterWindow_H)
+			{
+				curr_x += item.x;
+				if (min_x > item.x) min_x = item.x;
+				if (max_x < item.x) max_x = item.x;
+			}
+			curr_x /= n;
+			filterWindow_H.RemoveAt(0);
+		}
+		float height = max_x - min_x;
+
+
+		if (window_H.Count > window_H_count && height > minimun_width)
+		{
+			float mean_x = 0.0f;
+			int n = window_H.Count;
+			foreach (var item in window_H)
+			{
+				mean_x += item.x;
+			}
+			mean_x /= n;
+
+			float sign1_x = GetSign(prev_x, mean_x);
+			float sign2_x = GetSign(curr_x, mean_x);
+
+			if (sign1_x != sign2_x)
+			{
+				zeroCrossings_H += 1;
+			}
+
+			window_H.RemoveAt(0);
+		}
+		Debug.Log(filterWindow_H.Count + "/" + min_x + "/" + max_x + "/" + height + "/" + zeroCrossings_H);
+
+		prev_x = curr_x;
 	}
 }
