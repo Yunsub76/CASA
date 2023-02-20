d1e237ef6b7d9b4528970f6f1f5e56433e0c6116
DESKTOP-VD3EJSA\user01 <theung16@handong.ac.kr>
2023-02-20 14:34:25 +0900

    잔 오류 수정

--- CASA/Assets/Scripts/ScoreManager.cs
+++ CASA/Assets/Scripts/ScoreManager.cs
@@ -52,7 +52,7 @@ public class ScoreManager : MonoBehaviour {
             teamBalance = 1;
         }
 
-        if ((score >= 4700 && score < 6000) || sliderTimer.gameTime2 > sliderTimer.gameTime-1) //최종 시간에 도달하거나 4700점에 도달할 시 마지막 미션 등장
+        if ((score >= 4700 && score < 6000) || sliderTimer.gameTime2 > 10) //최종 시간에 도달하거나 4700점에 도달할 시 마지막 미션 등장
         {
             NumCircle = HandObjectArray[0].GetComponent<HandTracker>().numOfCircles + HandObjectArray[1].GetComponent<HandTracker>().numOfCircles + HandObjectArray[2].GetComponent<HandTracker>().numOfCircles;
             GaugeArray[2].GetComponent<Slider>().value = NumCircle;
@@ -71,7 +71,7 @@ public class ScoreManager : MonoBehaviour {
                     FinishThirdEvent();
             }
         }
-        else if(score >= 2500 || sliderTimer.gameTime2 > 60) //60초 후나 2500점 도달하면 두번째 미션 등장
+        else if(score >= 2500) //2500점 도달하면 두번째 미션 등장
         {
             NumVertical = HandObjectArray[0].GetComponent<HandTracker>().zeroCrossings + HandObjectArray[1].GetComponent<HandTracker>().zeroCrossings + HandObjectArray[2].GetComponent<HandTracker>().zeroCrossings;
             GaugeArray[1].GetComponent<Slider>().value = NumVertical-6;
@@ -84,7 +84,7 @@ public class ScoreManager : MonoBehaviour {
                 FinishSecondEvent();
             }
         }
-        else if(score >= 600) //30초 후나 600점 도달하면 두번째 미션 등장
+        else if(score >= 600) //600점 도달하면 두번째 미션 등장
         {
             NumHorizontal = HandObjectArray[0].GetComponent<HandTracker>().zeroCrossings_H + HandObjectArray[1].GetComponent<HandTracker>().zeroCrossings_H + HandObjectArray[2].GetComponent<HandTracker>().zeroCrossings_H;
             GaugeArray[0].GetComponent<Slider>().value = NumHorizontal;
@@ -118,7 +118,6 @@ public class ScoreManager : MonoBehaviour {
         MissionUIArray[0].SetActive(false);
         score += 1000;
         generateFallingTrash.IsMissionTime = false;
-        activateUI.IsGaming = false;
     }
 
     void ActiveSecondEvent()
@@ -161,5 +160,6 @@ public class ScoreManager : MonoBehaviour {
         MissionUIArray[2].SetActive(false);
         score += 1000;
         generateFallingTrash.IsMissionTime = false;
+		activateUI.IsGaming = false;
     }
 }
