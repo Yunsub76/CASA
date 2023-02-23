63b6cd2489b6c6b2e2d6cd93ee76c1c100e7e757
DESKTOP-VD3EJSA\user01 <theung16@handong.ac.kr>
2023-02-23 11:51:23 +0900

    원 도우미 코드 추가

--- CASA/Assets/Scripts/ScoreManager.cs
+++ CASA/Assets/Scripts/ScoreManager.cs
@@ -15,7 +15,7 @@ public class ScoreManager : MonoBehaviour {
     [SerializeField] private GameObject[] MissionUIArray = new GameObject[3];
     private bool[] MissionBoolArray = new bool[3] { false, false, false };
 
-    [SerializeField] private GameObject[] HandObjectArray = new GameObject[3];
+    [SerializeField] private GameObject[] HandObjectArray = new GameObject[4];
 
     [SerializeField] private GameObject[] PositiveEndingObjectArray = new GameObject[3];
     [SerializeField] private GameObject[] NagativeEndingObjectArray = new GameObject[3];
@@ -111,7 +111,7 @@ public class ScoreManager : MonoBehaviour {
 
         if ((score >= 4500 || sliderTimer.gameTime2 > sliderTimer.gameTime -1) && OpenThirdMission == true) //최종 시간에 도달하거나 4700점에 도달할 시 마지막 미션 등장
         {
-            NumCircle = HandObjectArray[0].GetComponent<HandTracker>().numOfCircles + HandObjectArray[1].GetComponent<HandTracker>().numOfCircles + HandObjectArray[2].GetComponent<HandTracker>().numOfCircles;
+            NumCircle = HandObjectArray[0].GetComponent<HandTracker>().numOfCircles + HandObjectArray[1].GetComponent<HandTracker>().numOfCircles + HandObjectArray[2].GetComponent<HandTracker>().numOfCircles + HandObjectArray[3].GetComponent<HandTracker>().numOfCircles;
             GaugeArray[2].GetComponent<Slider>().value = NumCircle;
             
             if (generateFallingTrash.IsMissionTime == false)
@@ -123,7 +123,7 @@ public class ScoreManager : MonoBehaviour {
                 LoadCircularUI.SetActive(false);
                 LoadReverseCircularUI.SetActive(true);
 
-                NumRCircle = HandObjectArray[0].GetComponent<HandTracker>().numOfRCircles + HandObjectArray[1].GetComponent<HandTracker>().numOfRCircles + HandObjectArray[2].GetComponent<HandTracker>().numOfRCircles;
+                NumRCircle = HandObjectArray[0].GetComponent<HandTracker>().numOfRCircles + HandObjectArray[1].GetComponent<HandTracker>().numOfRCircles + HandObjectArray[2].GetComponent<HandTracker>().numOfRCircles + HandObjectArray[3].GetComponent<HandTracker>().numOfRCircles;
                 GaugeArray[2].GetComponent<Slider>().value = 5 * teamBalance + NumRCircle;
                 if (NumRCircle >= 5 * teamBalance)
                     FinishThirdEvent();
@@ -131,7 +131,7 @@ public class ScoreManager : MonoBehaviour {
         }
         else if(score >= 2500 && OpenSecondMission == true) //60초 후나 2500점 도달하면 두번째 미션 등장
         {
-            NumVertical = HandObjectArray[0].GetComponent<HandTracker>().zeroCrossings + HandObjectArray[1].GetComponent<HandTracker>().zeroCrossings + HandObjectArray[2].GetComponent<HandTracker>().zeroCrossings;
+            NumVertical = HandObjectArray[0].GetComponent<HandTracker>().zeroCrossings + HandObjectArray[1].GetComponent<HandTracker>().zeroCrossings + HandObjectArray[2].GetComponent<HandTracker>().zeroCrossings + HandObjectArray[3].GetComponent<HandTracker>().zeroCrossings;
             GaugeArray[1].GetComponent<Slider>().value = NumVertical;
             
             if (generateFallingTrash.IsMissionTime == false)
@@ -145,7 +145,7 @@ public class ScoreManager : MonoBehaviour {
         }
         else if(score >= 600 && OpenFirstMission == true) //30초 후나 600점 도달하면 두번째 미션 등장
         {
-            NumHorizontal = HandObjectArray[0].GetComponent<HandTracker>().zeroCrossings_H + HandObjectArray[1].GetComponent<HandTracker>().zeroCrossings_H + HandObjectArray[2].GetComponent<HandTracker>().zeroCrossings_H;
+            NumHorizontal = HandObjectArray[0].GetComponent<HandTracker>().zeroCrossings_H + HandObjectArray[1].GetComponent<HandTracker>().zeroCrossings_H + HandObjectArray[2].GetComponent<HandTracker>().zeroCrossings_H + HandObjectArray[3].GetComponent<HandTracker>().zeroCrossings_H;
             GaugeArray[0].GetComponent<Slider>().value = NumHorizontal-6;
             
             if (generateFallingTrash.IsMissionTime == false)
